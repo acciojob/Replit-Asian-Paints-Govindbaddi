1f033e3cdafd97c0c32620c69fa95c10cc095428
Govindbaddi <baddigovindarao710@gmail.com>
2025-09-10 12:10:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const wantedbox=document.querySelector(".wholeform")
+wantedbox.addEventListener("submit",addingcolor)
+function addingcolor(e) {
+	e.preventDefault()
+	const selectedbox=e.target[0].value
+	const selectedcolor=e.target[1].value
+	const boxIsSelected=document.getElementById("selectedbox")
+	console.log(boxIsSelected)
+	boxIsSelected.Style.backgroundColor=selectedcolor
+	
+}
